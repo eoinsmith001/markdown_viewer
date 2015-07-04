2099083dfd6b63f930f518056399c832dcebde7c
esmith <eoinsmith001@gmail.com>
2015-07-04 23:50:00 +0100

    added sharejs and redis

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Real Time Markdown Viewer
 
 https://scotch.io/tutorials/building-a-real-time-markdown-viewer
+
+- Requires redis
+
+## Run
+
+```
+redis-server > redis.log 2>&1 &
+node server.js
+```
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-var express = require('express');
+var express = require('express'),
+    sharejs = require('share');
+require('redis');
+
+// sharejs options
+var options = {
+  db: {
+    type: 'redis'
+  }
+};
 
 var app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req,res) {
   res.render('pad');
 });
+
+app.get('/(:id)', function(req,res) {
+  res.render('pad');
+});
+
+sharejs.server.attach(app,options);
 
 var port = process.env.PORT || 8080;
 
